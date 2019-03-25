91bdef578df88303381eff537f100f8392d32b18
alvarodaniel294 <alvarodaniel294@gmail.com>
2019-03-25 09:33:09 -0400

    localStorage in service:deseos

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,15 +8,24 @@ export class DeseosService {
 
   listas:Lista[]=[];
   constructor( ) {
-    const lista1 = new Lista("Recolectar Piedras del Infinito");
-    const lista2 = new Lista("heroes a desaparecer");
-
-    this.listas.push(lista1,lista2);
-    console.log(this.listas);
+    this.cargarStorage();
     
    }
    crearLista(titulo:string){
     const nuevaLista=new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+   }
+   guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas) );
+   }
+   cargarStorage(){
+     if( localStorage.getItem('data') ){
+        this.listas= JSON.parse( localStorage.getItem('data') )
+       
+     }else{
+       this.listas=[];
+     }
+
    }
 }
